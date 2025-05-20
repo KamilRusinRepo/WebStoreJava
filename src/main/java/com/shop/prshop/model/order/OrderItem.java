8ba2139b774e83be07498fa92c37821ad6f5cac6
kamil <kamil.rusin15@wp.pl>
2025-05-20 19:57:58 +0200

    Add OrderItem entity

---
+++ src/main/java/com/shop/prshop/model/order/OrderItem.java
@@ -0,0 +1,129 @@
+package com.shop.prshop.model.order;
+
+import com.shop.prshop.controller.AdminController;
+import jakarta.persistence.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+@Entity
+@Table(name="order_items")
+public class OrderItem {
+
+    private static final Logger logger = LoggerFactory.getLogger(OrderItem.class);
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "order_item_id")
+    private Long orderItemId;
+
+    @Column(name = "item_id")
+    private Long itemId;
+
+    @Column(name = "amount")
+    private int amount;
+
+    @Column(name = "sum")
+    private BigDecimal price;
+
+    @Column(name = "item_full_name")
+    private String itemFullName;
+
+    @Column(name = "item_image")
+    private String itemImage;
+
+    @Column(name = "order_date")
+    private String orderDate;
+
+    @Column(name = "order_id")
+    private Long orderId;
+
+    public OrderItem() {
+
+    }
+
+    public OrderItem(Long itemId, int amount, BigDecimal price, String itemFullName, String itemImage, Long orderId) {
+        this.itemId = itemId;
+        this.amount = amount;
+        this.price = price;
+        this.itemFullName = itemFullName;
+        this.itemImage = itemImage;
+        this.orderDate = formatOrderDate(LocalDateTime.now());
+        this.orderId = orderId;
+    }
+
+    private String formatOrderDate(LocalDateTime now) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
+        String formattedDateTime = now.format(formatter);
+        return formattedDateTime;
+    }
+
+    public Long getOrderItemId() {
+        return orderItemId;
+    }
+
+    public void setOrderItemId(Long orderItemId) {
+        this.orderItemId = orderItemId;
+    }
+
+
+    public Long getItemId() {
+        return itemId;
+    }
+
+    public void setItemId(Long itemId) {
+        this.itemId = itemId;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public Long getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(Long orderId) {
+        this.orderId = orderId;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public String getItemFullName() {
+        return itemFullName;
+    }
+
+    public void setItemFullName(String itemFullName) {
+        this.itemFullName = itemFullName;
+    }
+
+    public String getItemImage() {
+        return itemImage;
+    }
+
+    public void setItemImage(String itemImage) {
+        this.itemImage = itemImage;
+    }
+
+    public String getOrderDate() {
+        return orderDate;
+    }
+
+    public void setOrderDate(String orderDate) {
+        this.orderDate = orderDate;
+    }
+
+}
