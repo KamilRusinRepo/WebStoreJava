c3a2bbc2f9f494ff27ac8229401261f2f93f9450
kamil <kamil.rusin15@wp.pl>
2025-05-24 22:17:13 +0200

    Add OrderService

---
+++ src/main/java/com/shop/prshop/service/OrderService.java
@@ -0,0 +1,75 @@
+package com.shop.prshop.service;
+
+import com.shop.prshop.Cart;
+import com.shop.prshop.CartItem;
+import com.shop.prshop.controller.OrderController;
+import com.shop.prshop.dto.OrderDto;
+import com.shop.prshop.mapper.OrderMapper;
+import com.shop.prshop.model.order.Order;
+import com.shop.prshop.model.order.OrderItem;
+import com.shop.prshop.model.user.User;
+import com.shop.prshop.repository.OrderItemRepository;
+import com.shop.prshop.repository.OrderRepository;
+import com.shop.prshop.repository.UserRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class OrderService {
+    private final Cart cart;
+    private final OrderRepository orderRepository;
+    private final OrderItemRepository orderItemRepository;
+    private final UserRepository userRepository;
+    private static final Logger logger = LoggerFactory.getLogger(OrderService.class);
+
+    @Autowired
+    public OrderService(Cart cart, OrderRepository orderRepository, OrderItemRepository orderItemRepository, UserRepository userRepository) {
+        this.cart = cart;
+        this.orderRepository = orderRepository;
+        this.orderItemRepository = orderItemRepository;
+        this.userRepository = userRepository;
+    }
+
+    public void saveOrder(OrderDto orderDto) {
+        Order order = OrderMapper.mapToOrder(orderDto);
+        orderRepository.save(order);
+        orderItemRepository.saveAll(OrderMapper.mapToOrderItemList(cart, order));
+        cart.clearCart();
+    }
+
+    public OrderDto orderLoginInChecker(){
+        OrderDto orderDto = new OrderDto();
+        User user;
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        if(authentication != null && !authentication.getName().equals("anonymousUser")) {
+            String email = authentication.getName();
+            logger.info("email: " + email);
+            Optional<User> optionalUser = userRepository.findByEmail(email);
+            if(optionalUser.isPresent()) {
+                user = optionalUser.get();
+                orderDto = OrderMapper.mapToOrderDto(user);
+            }
+        }
+
+        return orderDto;
+    }
+
+    public List<OrderItem> findUserOrders(String email) {
+        List<Order> orders = orderRepository.findOrderByEmail(email);
+        List<OrderItem> orderItemsList = new ArrayList<>();
+        for(Order newOrder : orders) {
+            orderItemsList.addAll(orderItemRepository.findByOrderId(newOrder.getOrderId()));
+        }
+        return orderItemsList;
+    }
+}
