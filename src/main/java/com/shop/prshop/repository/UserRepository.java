631c49e77a573e02651ed3352a452753e62f06f2
Kamil Rusin <kamil.rusin15@wp.pl>
2025-05-26 22:05:15 +0200

    Add UserRepository

---
+++ src/main/java/com/shop/prshop/repository/UserRepository.java
@@ -0,0 +1,17 @@
+package com.shop.prshop.repository;
+
+import com.shop.prshop.model.user.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
+    @Query("SELECT u FROM User u WHERE u.email = ?1")
+    Optional<User> findByEmail(String email);
+
+    @Query("SELECT u FROM User u WHERE u.verificationCode = ?1")
+    Optional<User> findByVerificationCode(String verificationCode);
+
+}
+
