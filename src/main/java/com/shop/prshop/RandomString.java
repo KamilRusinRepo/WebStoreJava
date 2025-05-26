05fb4d715f4903b93d8bbe819cd67ce185c12b13
kamil <kamil.rusin15@wp.pl>
2025-05-26 19:24:59 +0200

    Add RandomString class to generate verification codes

---
+++ src/main/java/com/shop/prshop/RandomString.java
@@ -0,0 +1,20 @@
+package com.shop.prshop;
+
+import java.util.Random;
+
+public class RandomString {
+
+    private static final String CHARACTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+    public static String generate(int number) {
+
+        StringBuilder sb = new StringBuilder(number);
+        Random random = new Random();
+        for (int i = 0; i < number; i++) {
+            int randomIndex = random.nextInt(CHARACTERS.length());
+            char randomChar = CHARACTERS.charAt(randomIndex);
+            sb.append(randomChar);
+        }
+        return sb.toString();
+    }
+}
