ae8eaa468c4427b3341cfbd866eb9dcf0f972a3e
Kamil Rusin <kamil.rusin15@wp.pl>
2025-05-27 21:51:25 +0200

    Change user ID type from int to Long in userRepository

--- src/main/java/com/shop/prshop/repository/UserRepository.java
+++ src/main/java/com/shop/prshop/repository/UserRepository.java
@@ -6,7 +6,7 @@ import org.springframework.data.jpa.repository.Query;
 
 import java.util.Optional;
 
-public interface UserRepository extends JpaRepository<User, Integer> {
+public interface UserRepository extends JpaRepository<User, Long> {
     @Query("SELECT u FROM User u WHERE u.email = ?1")
     Optional<User> findByEmail(String email);
 
