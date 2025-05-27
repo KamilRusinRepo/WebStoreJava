d8b8b25112eefabe67d81bf01e42636c8c968295
Kamil Rusin <kamil.rusin15@wp.pl>
2025-05-27 23:18:57 +0200

    Add tests for OrderItem Entity

---
+++ src/test/java/com/shop/prshop/OrderItemTest.java
@@ -0,0 +1,55 @@
+package com.shop.prshop;
+
+import com.shop.prshop.model.order.OrderItem;
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class OrderItemTest {
+    @Test
+    void testNoArgsConstructorAndSetters() {
+        OrderItem orderItem = new OrderItem();
+        orderItem.setOrderItemId(1L);
+        orderItem.setItemId(2L);
+        orderItem.setAmount(3);
+        orderItem.setPrice(BigDecimal.valueOf(99.99));
+        orderItem.setItemFullName("Test Product");
+        orderItem.setItemImage("image.jpg");
+        orderItem.setOrderDate("2024-01-01 10:00");
+        orderItem.setOrderId(5L);
+
+        assertEquals(1L, orderItem.getOrderItemId());
+        assertEquals(2L, orderItem.getItemId());
+        assertEquals(3, orderItem.getAmount());
+        assertEquals(BigDecimal.valueOf(99.99), orderItem.getPrice());
+        assertEquals("Test Product", orderItem.getItemFullName());
+        assertEquals("image.jpg", orderItem.getItemImage());
+        assertEquals("2024-01-01 10:00", orderItem.getOrderDate());
+        assertEquals(5L, orderItem.getOrderId());
+    }
+
+    @Test
+    void testAllArgsConstructor() {
+        OrderItem orderItem = new OrderItem(
+                10L,
+                2,
+                BigDecimal.valueOf(50.00),
+                "Laptop Lenovo",
+                "laptop.jpg",
+                20L
+        );
+
+        assertEquals(10L, orderItem.getItemId());
+        assertEquals(2, orderItem.getAmount());
+        assertEquals(BigDecimal.valueOf(50.00), orderItem.getPrice());
+        assertEquals("Laptop Lenovo", orderItem.getItemFullName());
+        assertEquals("laptop.jpg", orderItem.getItemImage());
+        assertEquals(20L, orderItem.getOrderId());
+
+        String orderDate = orderItem.getOrderDate();
+        assertNotNull(orderDate);
+        assertTrue(orderDate.matches("\\d{4}-\\d{2}-\\d{2} \\d{2}:\\d{2}"));
+    }
+}
